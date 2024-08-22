8bd4b94e56af024c2bc9700b61ed84d111be7c58
Juan Valen <al.valen@hotmail.com>
2024-08-22 10:21:32 +0300

    Create 03_suorakulma_geometria.py

---
+++ 00_tehtavat/02_Muuttujat_ja_vuorovaikutteiset_ohjelmat/03_suorakulma_geometria.py
@@ -0,0 +1,12 @@
+print("Suorakulma:")
+width = float(input("  Leveys: "))
+height = float(input("  Korkeus: "))
+surface = (height*2) + (width*2)
+area = height*width
+print()
+print("Suorakulma: ")
+print("  piiri on %.2fm" % (surface))
+print("  pinta-ala on %.2fm2" % (area))
+
+
+2
