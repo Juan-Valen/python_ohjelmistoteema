a9db4b9103ea6f302ec6a0a93ed476cc68504aa6
Juan-Valen <al.valen@hotmail.com>
2024-10-26 11:19:29 +0300

    Create 02_building.py

---
+++ 00_tehtavat/10_oobs/02_building.py
@@ -0,0 +1,70 @@
+# IDEA
+# Jatka edellisen tehtävän ohjelmaa siten, että teet Talo-luokan. 
+# Talon alustajaparametreina annetaan alimman ja ylimmän kerroksen numero sekä hissien lukumäärä. 
+# Talon luonnin yhteydessä talo luo tarvittavan määrän hissejä. 
+# Hissien lista tallennetaan talon ominaisuutena. 
+# Kirjoita taloon metodi aja_hissiä, joka saa parametreinaan hissin numeron ja kohdekerroksen. 
+# Kirjoita pääohjelmaan lauseet talon luomiseksi ja talon hisseillä ajelemiseksi.
+
+import os
+from random import randint
+from typing import List
+# INTRO
+os.system("cls")
+class Elevator:
+    def __init__(self, bottom: int, top: int):
+        self.floor_bottom = -bottom
+        self.floor_top = top
+        self.current_floor = -bottom
+        print('Elevator has been configurate.')
+        print('Starting program.')
+        print(f'current floor is {self.current_floor}')
+
+    def go_to_floor(self, floor: int):
+        if floor < self.floor_bottom or floor > self.floor_top:
+            return
+        print(f'Current floor is {self.current_floor}')
+        while floor != self.current_floor:
+            if floor > self.current_floor:
+                self.floor_up()
+            else:
+                self.floor_down()
+
+    def floor_up(self):
+        prospect_floor = self.current_floor+1
+        if prospect_floor > self.floor_top:
+            return
+        self.current_floor = prospect_floor
+        print(f'current floor is {prospect_floor}')
+        
+    def floor_down(self):
+        prospect_floor = self.current_floor-1
+        if prospect_floor < self.floor_bottom:
+            return
+        self.current_floor = prospect_floor
+        print(f'current floor is {prospect_floor}')
+class Building:
+    def __init__(self, bottom: int, top: int, elevators: int):
+        self.elevators = []
+        print(f"Starting constructions")
+        print(f"Starting installation of elevators")
+        for n in range(elevators):
+            self.elevators.append(Elevator(bottom, top))
+        print(f"Max elevators: {len(self.elevators)}")
+
+    def run_elevator(self, elevator: int, floor: int):
+        if elevator < 1 or elevator > len(self.elevators):
+            return
+        print(f"Elevator {elevator} moving.")
+        self.elevators[elevator-1].go_to_floor(floor)
+
+# INPUT
+# LOGIC
+b = Building(2, 5, 5)
+
+b.run_elevator(1,3)
+# OUTPUT
+
+#EXIT
+input("\n\npress ENTER to exit")
+os.system("cls")
